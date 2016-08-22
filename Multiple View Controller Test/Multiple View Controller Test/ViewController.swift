c38309e2c8e1205e22ecc10e5309a30eadf014da
Mohib Ibrahim <mohibian@Mohibs-MacBook-Air.local>
2016-08-21 23:40:04 -0500

    Added Learning of multiple View controllers

---
+++ Multiple View Controller Test/Multiple View Controller Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiple View Controller Test
+//
+//  Created by Mohib Ibrahim on 8/21/16.
+//  Copyright © 2016 Mohib Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
